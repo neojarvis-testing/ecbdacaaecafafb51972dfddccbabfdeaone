6627557096e7b8153b9b84e9a575b109a4233385
neojarvis <techadmin@iamneo.ai>
2025-06-26 10:46:41 +0000

    Manual-save-commit-6/26/2025--4:16:36-PM

--- springapp/src/main/java/com/examly/springapp/configurations/ApplSecurityConfig.java
+++ springapp/src/main/java/com/examly/springapp/configurations/ApplSecurityConfig.java
@@ -4,20 +4,13 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebMvc
 
 public class ApplSecurityConfig {
 
